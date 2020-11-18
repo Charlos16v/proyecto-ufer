26ddcb4c145a9f63c41d9b2f2d354222a2626eb4
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-11-18 22:00:40 +0100

    improvement: web crawler

--- src/GetContent.py
+++ src/GetContent.py
@@ -2,13 +2,21 @@ import requests
 from requests.models import Response
 
 #Funcion encargada de hacer un request y obtener el response en forma de string.
-def GetContent(url):
-    contenido = requests.get(url)
-    return (contenido.content)
+#def get_content(url):
+#    contenido = requests.get(url)
+#    return (contenido.text)
 
 
 #Test rapido no tener en cuenta.
-print (GetContent('http://127.0.0.1:5500/testscrapper.html'))
+#print (get_content('http://127.0.0.1:5500/testscrapper.html'))
 
-def get_content(url):
+#from http.client import HTTPConnection
+
+
+#def get_content(url):
+#    conexion = HTTPConnection(url)
+#    conexion.request("GET","/")
+#    resultado = conexion.getresponse()
+#    contenido = resultado.read()
+#    return contenido
 
