e9c1402c6d7968b288dbdf43099c6d11ba2cc7f7
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-11-19 17:28:57 +0100

    refactor: testing

--- src/pruebas.py
+++ src/pruebas.py
@@ -4,15 +4,15 @@ import requests
 page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
 tree = html.fromstring(page.content)
 
-#This will create a list of buyers:
+# This will create a list of buyers:
 buyers = tree.html('//div[@title="buyer-name"]/text()')
-#This will create a list of prices
+# This will create a list of prices
 prices = tree.xpath('//span[@class="item-price"]/text()')
 
 print (buyers)
 print (prices)
 
 
-https://127.0.1.1:5000
-../html/services.html
-./index.html
+# https://127.0.1.1:5000
+# ../html/services.html
+# ./index.html
