b1a38f02b84f5b2397a35f1416d162a0f3470944
Carlos Uriel <cdominguez@cifpfbmoll.eu>
2020-11-24 13:20:31 +0100

    build: scrapper

--- src/scrapper.py
+++ src/scrapper.py
@@ -7,20 +7,21 @@ urls=crawl_web("http://127.0.0.1:5500/ufer/index.html",3)
 #for i in urls:
 
 
-contenido=get_page("http://127.0.0.1:5500/ufer/ufer_gold.html")
+#contenido=get_page("http://127.0.0.1:5500/ufer/ufer_gold.html")
 
 
-def get_next_target(page):
-    start_link = page.find('<h3 id="name"')
-    if start_link == -1:
-        return None, 0
-    start_quote = page.find('"', start_link)
-    end_quote = page.find('"', start_quote + 1)
-    url = page[start_quote + 1:end_quote]
-    return url, end_quote
+
+#def get_next_target(page):
+#    start_link = page.find('<h3 id="name"')
+#    if start_link == -1:
+#        return None, 0
+#   start_quote = page.find('"', start_link)
+#   end_quote = page.find('"', start_quote + 1)
+#   url = page[start_quote + 1:end_quote]
+#   return url, end_quote
 
 
-name=contenido.find('<h3 id="name"')
+#name=contenido.find('<h3 id="name"')
 
 
 
