bd3057e17a12816112c003de88f27cf6aa76158b
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-11-25 20:00:37 +0100

    fix: import requests

--- src/get_content.py
+++ src/get_content.py
@@ -1,3 +1,4 @@
+import requests
 # Funcion encargada de hacer un request y obtener el response en forma de string a partir de <body>.
 def get_content(url):
     contenido = requests.get(url).text.replace("\r\n","\r").replace("\r","").replace("\n","").replace("\t","")
