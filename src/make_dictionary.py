98b0ab287aeda5870c44d608ae9a4bf5672cbda3
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-11-25 20:59:24 +0100

    refactor(make_dictonary): return without save it in a variable

--- src/make_dictionary.py
+++ src/make_dictionary.py
@@ -1,5 +1,8 @@
-def make_dictionary(a, b):
-    dictionary = dict(zip(a, b))
-    return dictionary
+from get_values import UFER_KEYS,get_values
+from get_content import get_content
 
-#print(make_dictionary(UFER_KEYS, get_values(get_content("https://charlos16v.github.io/proyecto-ufer/ufer_bus.html"))))
+def make_dictionary(a, b):
+    return dict(zip(a, b))
+
+
+print(make_dictionary(UFER_KEYS, get_values(get_content("https://charlos16v.github.io/proyecto-ufer/ufer_bus.html"))))
