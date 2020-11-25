de045b1d3636fb283d172d66cb72377a4943a15b
Carlos Uriel <cdominguez@cifpfbmoll.eu>
2020-11-25 22:42:23 +0100

    fix(make_dictonary.py): bug imports

--- src/make_dictionary.py
+++ src/make_dictionary.py
@@ -1,8 +1,3 @@
-from get_values import UFER_KEYS,get_values
-from get_content import get_content
-
 def make_dictionary(a, b):
     return dict(zip(a, b))
 
-
-print(make_dictionary(UFER_KEYS, get_values(get_content("https://charlos16v.github.io/proyecto-ufer/ufer_bus.html"))))
