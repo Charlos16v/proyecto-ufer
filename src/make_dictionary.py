7bd7f26956a081c5ffb39e9e8ebf1f1d2f78a45d
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-11-25 19:54:58 +0100

    build(make_dictionary.py): WIP

---
+++ src/make_dictionary.py
@@ -0,0 +1,5 @@
+def make_dictionary(a, b):
+    dictionary = dict(zip(a, b))
+    return dictionary
+
+#print(make_dictionary(UFER_KEYS, get_values(get_content("https://charlos16v.github.io/proyecto-ufer/ufer_bus.html"))))
