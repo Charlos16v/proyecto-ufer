4b6f55ed1e98c2844dc3ba62e39b3f64c503a08a
Carlos Uriel <cdominguez@cifpfbmoll.eu>
2020-11-25 22:40:31 +0100

    build(get_values_conf.py): modulo unicamente dedicado a la configuración de los parametros a scrappear

---
+++ src/get_values_conf.py
@@ -0,0 +1,7 @@
+# CONFIGURACIÓN PARA EL SCRAPPER DE UFER
+
+# En "UFER_TYPE" se define el respectivo type del contenido a scrappear.
+UFER_TYPE = [str, str, str, int, str, str, str, int, int, list]
+
+# En "UFER_KEYS" se define el respectivo identificador del contenido a scrappear.
+UFER_KEYS = ['name', 'description', 'driver', 'passengers', 'privacy', 'seats', 'propulsion', 'top_speed', 'price', 'perico']
