15b23837d5ba7e1b793e6596c7bd012b9ce05a8e
Carlos Uriel <cdominguez@cifpfbmoll.eu>
2020-11-25 23:34:23 +0100

    fix(get_values_conf.py): change value of a key

--- src/get_values_conf.py
+++ src/get_values_conf.py
@@ -4,4 +4,4 @@
 UFER_TYPE = [str, str, str, int, str, str, str, int, int, list]
 
 # En "UFER_KEYS" se define el respectivo identificador del contenido a scrappear.
-UFER_KEYS = ['name', 'description', 'driver', 'passengers', 'privacy', 'seats', 'propulsion', 'top_speed', 'price', 'perico']
+UFER_KEYS = ['name', 'description', 'driver', 'passengers', 'privacy', 'seats', 'propulsion', 'top_speed', 'price', 'amenities']
