ff9e7d324318493dd6fc31708cac0f55b0662b42
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-11-26 03:18:17 +0100

    refactor(get_all_values.py): puts the scrapped data for each page in a document in a collection of a Database in MongoDB

--- src/get_all_values.py
+++ src/get_all_values.py
@@ -1,17 +1,22 @@
 from get_values import get_values
 from get_content import get_content
+from get_values_conf import UFER_KEYS
+from make_dictionary import make_dictionary
+from web_crawler import crawl_web
+from to_mongo import to_mongo
+
 # En Obras
-#links=crawl_web("https://charlos16v.github.io/proyecto-ufer/",3)
+links=crawl_web("https://charlos16v.github.io/proyecto-ufer/",3)
 ###
 
 #print(links)
 
 ###
 def get_values_on_links(links):
-    datos=[]
-    for link in links:
-        datos.append((get_values(get_content(link))))
-    return datos
+    for url in links:
+        to_mongo(make_dictionary(UFER_KEYS,get_values(get_content(url))))
 ###
 
 #print(get_values_on_links(links))
+
+get_values_on_links(links)
