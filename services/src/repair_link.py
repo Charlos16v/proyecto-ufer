c31ca729a85d22139b5557a62cca21b6367e3db5
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-12-01 20:38:20 +0100

    refactor(repair_link.py): use constant ("UFER_DOMAIN")

--- services/src/repair_link.py
+++ services/src/repair_link.py
@@ -1,7 +1,10 @@
 # MEJORAR
 # Convierte las rutas relativas a otras pàginas, a una url "valida", con su correspondiente dominio.
+from services.src.get_values_conf import UFER_DOMAIN
+
+
 def repair_link(link):
     if link.find("http://") == -1:
-        link="https://charlos16v.github.io/proyecto-ufer/"+link
+        link=UFER_DOMAIN+link
         return link
     return link
