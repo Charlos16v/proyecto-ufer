c3bf1a54880076f56dd50deb4d132c359d58cb80
Carlos Uriel <cdominguez@cifpfbmoll.eu>
2020-12-01 22:40:46 +0100

    refactor(get_all_values.py): change url to "UFER_DOMAIN", and depth to "UFER_DEPTH"

--- services/src/get_all_values.py
+++ services/src/get_all_values.py
@@ -1,6 +1,6 @@
 from services.src.get_values import get_values
 from services.src.get_content import get_content
-from services.src.get_values_conf import UFER_KEYS
+from services.src.get_values_conf import UFER_KEYS,UFER_DOMAIN,UFER_DEPTH
 from services.src.make_dictionary import make_dictionary
 from services.src.web_crawler import crawl_web
 from services.src.to_mongo import to_mongo
@@ -19,4 +19,4 @@ def get_values_on_links(links):
 
 #print(get_values_on_links(links))
 
-get_values_on_links(crawl_web("https://charlos16v.github.io/proyecto-ufer/",3))
+get_values_on_links(crawl_web(UFER_DOMAIN,UFER_DEPTH))
