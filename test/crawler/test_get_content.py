f5eb27362228ac8aa2083154ac5f553f1836e835
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-12-03 16:06:56 +0100

    build(tests): add some tests

---
+++ test/crawler/test_get_content.py
@@ -0,0 +1,4 @@
+from src.services.crawler.get_content import get_content
+
+def test_get_content():
+    assert get_content("test")
---
+++ test/crawler/test_repair_link.py
@@ -0,0 +1,4 @@
+from src.services.crawler.repair_link import repair_link
+
+def test_repair_link():
+    assert repair_link("test")
