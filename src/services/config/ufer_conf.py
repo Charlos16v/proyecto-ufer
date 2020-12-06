6cad811c9ae3e7cf0826a3da29af1054a95f2fe8
Carlos Uriel <cdominguez@cifpfbmoll.eu>
2020-12-06 17:10:56 +0100

    refactor(ufer_conf): reorder constants

--- src/services/config/ufer_conf.py
+++ src/services/config/ufer_conf.py
@@ -1,13 +1,13 @@
 # CONFIGURACIÓN PARA EL SCRAPPER DE UFER
 
-# En "UFER_TYPE" se define el respectivo type del contenido a scrappear.
-UFER_TYPE = [str, str, str, int, str, str, str, int, int, list]
-
-# En "UFER_KEYS" se define el respectivo identificador del contenido a scrappear.
-UFER_KEYS = ['name', 'description', 'driver', 'passengers', 'privacy', 'seats', 'propulsion', 'top_speed', 'price', 'amenities']
-
 # En "UFER_DOMAIN" se declara el dominio de la web a scrappear.
 UFER_DOMAIN = "https://charlos16v.github.io/proyecto-ufer/"
 
 # En "UFER_DEPTH" se define la profundidad de la recursividad de búsqueda de links.
 UFER_DEPTH = 3
+
+# En "UFER_KEYS" se define el respectivo identificador del contenido a scrappear.
+UFER_KEYS = ['name', 'description', 'driver', 'passengers', 'privacy', 'seats', 'propulsion', 'top_speed', 'price', 'amenities']
+
+# En "UFER_TYPE" se define el respectivo type del contenido a scrappear.
+UFER_TYPE = [str, str, str, int, str, str, str, int, int, list]
