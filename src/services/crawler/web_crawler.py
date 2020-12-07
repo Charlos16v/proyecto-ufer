8efe0f0a85984f5132665a91077afa9d7a3b7066
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-12-07 22:08:21 +0100

    refactor: add some spaces to the code

--- src/services/crawler/web_crawler.py
+++ src/services/crawler/web_crawler.py
@@ -3,6 +3,7 @@ from src.services.crawler.repair_link import repair_link
 from src.services.crawler.get_content import get_content
 from src.services.config.ufer_conf import UFER_KEYS
 
+
 # get_next_target recorre el contenido HTML en busca de enlaces "<a href=" y devuelve enlaces y su posicion final.
 
 def get_next_target(page):
@@ -13,11 +14,11 @@ def get_next_target(page):
     end_quote = page.find('"', start_quote + 1)
     url = page[start_quote + 1:end_quote]
     return url, end_quote
-
+print(get_next_target("<a href=''"))
 
 # union se encarga de insertar el contenido de la lista "q" que no se encuentra en "p".
 # refactor
-def union(p,q):
+def union(p, q):
     for e in q:
         if e not in p:
             p.append(e)
@@ -28,9 +29,9 @@ def union(p,q):
 def get_all_links(page):
     links = []
     while True:
-        url,endpos = get_next_target(page)
+        url, endpos = get_next_target(page)
         if url:
-            parsed_url=repair_link(url)
+            parsed_url = repair_link(url)
             links.append(parsed_url)
             page = page[endpos:]
         else:
@@ -38,24 +39,24 @@ def get_all_links(page):
     return links
 
 
-#TESTEAR CAMBIO
+# TESTEAR CAMBIO
 def is_for_scrapp(link):
     for key in UFER_KEYS:
-        if get_content(link).find('id="' + str(key)+'">') == -1:
+        if get_content(link).find('id="' + str(key) + '">') == -1:
             return False
         else:
             continue
     return True
 
 
-def crawl_web(seed,max_depth):
+def crawl_web(seed, max_depth):
     tocrawl = [seed]
     crawled = []
     next_depth = []
     depth = 0
     while tocrawl and depth <= max_depth:
         page = tocrawl.pop()
-        if page not in crawled: #and is_for_scrapp(page) == True
+        if page not in crawled:  # and is_for_scrapp(page) == True
             union(next_depth, get_all_links(get_content(page)))
             if is_for_scrapp(page) == True:
                 crawled.append(page)
@@ -63,4 +64,3 @@ def crawl_web(seed,max_depth):
             tocrawl, next_depth = next_depth, []
             depth += 1
     return crawled
-
