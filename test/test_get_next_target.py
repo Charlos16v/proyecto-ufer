e5939681c185ba38b29e394a35e7023319718cf8
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-12-07 22:08:50 +0100

    test: add tests for get_next_target function

--- test/test_get_next_target.py
+++ test/test_get_next_target.py
@@ -4,4 +4,6 @@ import pytest
 
 @pytest.mark.get_next_target
 def test_get_next_target():
-    assert get_next_target()
+    assert get_next_target('<body> <a href="master"></a> <a href="test">') == ('master', 22)
+    assert get_next_target("") == (None, 0)
+    assert get_next_target("hello world") == (None, 0)
