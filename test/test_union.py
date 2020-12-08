8131d1e8fda998d3aa92d3e28c9707e2917325fc
JuampaMolina <jruiz@cifpfbmoll.eu>
2020-12-08 04:02:44 +0100

    test: casos test para funcion union

--- test/test_union.py
+++ test/test_union.py
@@ -1,6 +1,8 @@
-from src.services.crawler.union import union
-
+from src.services.crawler.web_crawler import union
+import pytest
 
 @pytest.mark.union
 def test_union():
-    assert union(['a', 'b', 'c'], ['c', 'd', 'e']) == ['a', 'b', 'c', 'd', 'e']
+    assert union(['a', 'b', 'c'], ['c', 'd', 'e']) == union(['a', 'b', 'c', 'd', 'e'], ['c', 'd', 'e'])
+    assert union([1, 2, 3], [3, 4, 5]) == union([1, 2, 3, 4, 5], [1, 2, 3])
+    assert union([], []) == union([], [])
