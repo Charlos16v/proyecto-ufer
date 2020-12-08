928b8a4f3b127325727b5788b3a277853662811f
Charlos16v <cdominguez@cifpfbmoll.eu>
2020-12-08 03:45:24 +0100

    refactor(ufer_conf): add an assert

--- src/services/config/ufer_conf.py
+++ src/services/config/ufer_conf.py
@@ -2,16 +2,16 @@
 
 # En "UFER_DOMAIN" se declara el dominio de la web a scrappear.
 UFER_DOMAIN = "https://charlos16v.github.io/proyecto-ufer/"
-#UFER_DOMAIN = "https://charlos16v.github.io/proyecto-ufer/test.html"
+
+
 # En "UFER_DEPTH" se define la profundidad de la recursividad de búsqueda de links.
 UFER_DEPTH = 3
 
+
 # En "UFER_KEYS" se define el respectivo identificador del contenido a scrappear.
 UFER_KEYS = ['name', 'description', 'driver', 'passengers', 'privacy', 'seats', 'propulsion', 'top_speed', 'price', 'amenities']
-#UFER_KEYS = ['name', 'description', 'price']
-#UFER_KEYS = ['asientos', 'descripcion', 'extras']
 
 # En "UFER_TYPE" se define el respectivo type del contenido a scrappear.
 UFER_TYPE = [str, str, str, int, str, str, str, int, int, list]
-#UFER_TYPE = [str, str, int ]
-#UFER_TYPE = [str, str, list]
+
+assert len(UFER_TYPE) == len(UFER_KEYS) # precondición para comprobar que las 2 listas tienen la misma longitud.
